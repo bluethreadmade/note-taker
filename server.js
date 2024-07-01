2e3934b0e07b5fbfbd672c8b932ec86a22c31b97
Megan McNeill <mcneim2@gmail.com>
2024-07-01 15:48:15 -0400

    added get route for notes data

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
 
 const PORT = process.env.port || 5501;
 
 const app = express();
 
-app.use('/api', api);
 app.use(express.static('public'));
 
-
+const notesData = require('./db/db.json');
 
 // GET Route for homepage
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
   );
 
+// GET Route for all notes data  
+app.get('/api/notes', (req, res) => res.json(notesData));
+
 // GET Route for homepage
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
   );
 
-// GET Route for notes data  
-app.get('/api/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json').then((data) => res.json(JSON.parse(data)))
-)
-});
+
+// // POST Route for new note
+// app.post('api/notes', (req, res) => {
+
+// })
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
