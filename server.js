ff532d8296fe4d2535cd06149c2ac3aa6b79d9f7
Megan McNeill <mcneim2@gmail.com>
2024-06-30 23:05:29 -0400

    adding route for notes data

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
   );
 
- 
+// GET Route for notes data  
+app.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json').then((data) => res.json(JSON.parse(data)))
+)
+});
+
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
 );
